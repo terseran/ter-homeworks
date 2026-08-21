03368ffb3ff5acf9b586e939641ab52c1d05a682
TSA <rrchip@ya.ru>
2026-08-21 18:22:14 +0300

    Homework 01

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -4,12 +4,10 @@ terraform {
       source  = "kreuzwerker/docker"
     }
   }
-  required_version = "~>1.12.0" /*Многострочный комментарий.
- Требуемая версия terraform */
+  required_version = "~>1.15.0" 
 }
 provider "docker" {}
 
-#однострочный комментарий
 
 resource "random_password" "random_string" {
   length      = 16
@@ -19,19 +17,18 @@ resource "random_password" "random_string" {
   min_numeric = 1
 }
 
-/*
-resource "docker_image" {
+resource "docker_image" "nginx"{
   name         = "nginx:latest"
   keep_locally = true
 }
 
-resource "docker_container" "1nginx" {
+resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string_FAKE.resulT}"
+  # name  = "example_${random_password.random_string.result}"
+  name  = "hello_world"
 
   ports {
     internal = 80
     external = 9090
   }
 }
-*/
